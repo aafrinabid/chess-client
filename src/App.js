3e54bf01b158deb48ba6a750846b2184801e8e6d
aafrinabid <mohdaafrin@gmail.com>
2022-10-08 17:37:08 +0530

    made the code work front end design costumization is pending

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios'
+import { useEffect, useState } from 'react';
 function App() {
+  const list=[]
+  const[ currentKnight,setCurrentKnight]=useState({})
+  const [possibleMoves,setPossibleMoves]=useState([])
+  console.log(currentKnight)
+    const findPossiblemoves=(i)=>{
+      console.log(i)
+      setCurrentKnight(i)
+      axios.post('http://localhost:4000/getmoves',i).then(res=>{
+       setPossibleMoves([...res.data])
+      })
+    }
+    const checkIfPossible=(i,j)=>{
+      const data=possibleMoves.find(e=>e.index==i && e.secondArrayIndex==j)
+      if(data!==undefined){
+        return true
+      }else{
+        return false
+      }
+    }
+
+  for(let i=0;i<8;i++){
+    for (var j = 0; j < 8; j++){
+      const isPossible=checkIfPossible(i,j)
+     list.push( <div key={`${i}${j}`} onClick={findPossiblemoves.bind(null,{i,j})} style={ {float: 'left',width: '50px',height: '50px',backgroundColor:`${i==currentKnight.i && j==currentKnight.j ?'red': isPossible?'green': (i+j)%2==0?'#000':'#fff'}`}}></div>)
+
+    }
+  }
+ 
+  
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style= {{paddingRight:'200px',paddingLeft:'563px',paddingTop:'50px'}}>
+    <div  style={{   width: '400px',
+      height: '400px'
+   }}>
+{list}
+    </div>
     </div>
   );
 }
